54577e51ccab1e2a7d15a557004a457dbbd1212a
brandondacosta4 <81047935+brandondacosta4@users.noreply.github.com>
2021-05-10 21:34:43 -0400

    Create Time to Decompress.cpp

---
+++ CCC/2019/Time to Decompress.cpp
@@ -0,0 +1,19 @@
+#include <iostream>;
+using namespace std;
+int l, n;
+char s;
+int main() {
+	cin >> l;
+	while (l--) {
+		cin >> n >> s;
+		string x = "";
+		for (int i = 0; i < n; i++) {
+			x += s;
+
+		}
+		cout << x << endl;
+
+	}
+
+
+}
