d198efad71fd085bfd46da22bd1115d9595fcf40
brandondacosta4 <81047935+brandondacosta4@users.noreply.github.com>
2021-05-29 22:21:26 -0400

    Create Print the Elements of a Linked List.cpp

---
+++ HackerRank/Print the Elements of a Linked List.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+void printLinkedList(SinglyLinkedListNode* head) {
+    while(head != NULL){
+        cout << head -> data << '\n';
+        head = head -> next;
+        
+    }
+    
+    
+
+}
