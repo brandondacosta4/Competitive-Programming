6dd93174f9de7ff83a814d7029e1d3ca97da907b
brandondacosta4 <81047935+brandondacosta4@users.noreply.github.com>
2021-07-03 20:39:29 -0400

    Create Again_twenty_five.cpp

---
+++ Code-Forces/Again_twenty_five.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string p;
+
+    cin >> p;
+
+    cout << "25" << endl;
+
+    
+}
+
+//this was interesting to try and solve haha
+
+
+
