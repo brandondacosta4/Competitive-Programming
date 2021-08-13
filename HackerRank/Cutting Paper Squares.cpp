2ad409e792499bc6350dc2a92111a26c2ca53ab0
brandondacosta4 <81047935+brandondacosta4@users.noreply.github.com>
2021-08-13 09:03:31 -0400

    Create Cutting Paper Squares.cpp

---
+++ HackerRank/Cutting Paper Squares.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+int main()
+{
+    long long n, m;
+    cin >> n >> m;
+    
+    cout << n * m -1;
+    
+    return 0;
+    
+    
+}
+
